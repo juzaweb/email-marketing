722d7a09adb81c0c15ce1f569f4a02c1e306a12a
juzaweb <dangtheanh16@gmail.com>
2026-01-29 22:13:04 +0700

    fix: Remove website_id from unique constraint on email_subscribers table

--- database/migrations/2026_01_07_094627_create_email_subscribers_table.php
+++ database/migrations/2026_01_07_094627_create_email_subscribers_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->enum('status', ['subscribed', 'unsubscribed', 'bounced'])->default('subscribed')->index();
             $table->datetimes();
 
-            $table->unique(['email', 'website_id']);
+            $table->unique(['email']);
         });
     }
 
